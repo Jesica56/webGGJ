84ed6f9d99fb6a33b9dc974ce298235efc672716
Jesica56 <martinezjesicanoelia56@gmail.com>
2026-01-19 19:57:11 -0300

    cambios en el script

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ navLinks.forEach(link => {
     }
   });
 });
+
+
+  const galeriaImgs = document.querySelectorAll('.galeria-img');
+  const modalImg = document.getElementById('galeriaModalImg');
+
+  galeriaImgs.forEach(img => {
+    img.addEventListener('click', () => {
+      modalImg.src = img.dataset.img;
+    });
+  });
