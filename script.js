cc07017b6a27fa4a8f61278a524b04ec051b19dd
Jesica56 <martinezjesicanoelia56@gmail.com>
2026-01-19 19:22:09 -0300

    cambios finales

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-const navbar = document.querySelector('.custom-navbar');
+  const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+const navbarCollapse = document.querySelector(".navbar-collapse");
 
-  window.addEventListener('scroll', () => {
-    navbar.classList.toggle('scrolled', window.scrollY > 50);
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    if (navbarCollapse.classList.contains("show")) {
+      new bootstrap.Collapse(navbarCollapse).hide();
+    }
   });
-
-
-
-  const galeriaImgs = document.querySelectorAll('.galeria-img');
-  const modalImg = document.getElementById('galeriaModalImg');
-
-  galeriaImgs.forEach(img => {
-    img.addEventListener('click', () => {
-      modalImg.src = img.dataset.img;
-    });
-  });
-
+});
